4ec69f66807fc63a1f242e39143376f4f623be62
Samir <samir@cern.ch>
2012-12-05 15:24:23 +0100

    adding FirstConditionSafeRun call. Fixes #2

--- src/python/Data.py
+++ src/python/Data.py
@@ -3,6 +3,7 @@ from WMCore.REST.Format import RawFormat
 from T0WmaDataSvc.DataHello import *
 from T0WmaDataSvc.DataRun import *
 from T0WmaDataSvc.DataExpressConfig import *
+from T0WmaDataSvc.DataFirstConditionSafeRun import *
 
 class Data(DatabaseRESTApi):
   """Server object for REST data access API."""
@@ -18,4 +19,6 @@ class Data(DatabaseRESTApi):
     self._add({ "hello": Hello(app, self, config, mount),
                 "runid": RunId(app, self, config, mount),
                 "run":   Run(app, self, config, mount),
-                "express_config": ExpressConfig(app, self, config, mount)})
+                "express_config": ExpressConfig(app, self, config, mount),
+                "firstconditionsaferun": FirstConditionSafeRun(app, self, config, mount)
+                })
